6fcf7bd47e0837fb7a0e43c675ff380cc8382f8d
info700700 <info700700@yandex.ru>
2022-02-19 16:02:42 +0300

    статус задачи

--- work/task.go
+++ work/task.go
@@ -4,8 +4,9 @@ package work
 type TaskStatus uint8
 
 const (
+	Wait       TaskStatus = 0 // в ожидании. Задачу нужно сделать когда-нибудь, но сейчас на эту задачу нет времени (ресурсов).
 	Planned    TaskStatus = 1
-	InProgress TaskStatus = 2
+	InProgress TaskStatus = 2 // в работе
 	Complete   TaskStatus = 3
 )
 
